44ed602e5bacb7ea2c3c3af291790bc5e8476b45
quimtorra <q.torra@gmail.com>
2023-05-13 19:59:44 +0200

    3 Tab Navigation

--- App.js
+++ App.js
@@ -2,24 +2,58 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from "./src/screens/Home";
+import Datafon from "./src/screens/Datafon";
+import Productes from "./src/screens/Productes";
 import Compte from "./src/screens/Compte";
+import { colors } from "./src/utils/colors";
 import Constants from "expo-constants";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-          animation: "slide_from_right",
-        }}
+      <Tab.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Datafon"
+        activeColor={colors.accent}
+        inactiveColor={colors.main}
+        barStyle={{ backgroundColor: "tomato" }}
       >
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Compte" component={Compte} />
-      </Stack.Navigator>
+        <Tab.Screen
+          name="Datafon"
+          component={Datafon}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons
+                name="calculator"
+                color={color}
+                size={26}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Productes"
+          component={Productes}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="apps" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Compte"
+          component={Compte}
+          options={{
+            tabBarIcon: ({ color }) => {
+              <MaterialCommunityIcons name="account" color={color} size={26} />;
+            },
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
