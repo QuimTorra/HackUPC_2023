c886f0a2da921b4199fa5675c7e5c1e5658179c0
JorgeBorras18 <jorge.borras@estudiantat.upc.edu>
2023-05-13 16:54:51 +0200

    hey

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,23 @@
 import { Button, Text } from "react-native";
 import Pantalla from "../components/Pantalla";
+<<<<<<< Updated upstream
 import TopBar from "../components/TopBar";
+=======
+import NavButton from "../components/NavButton";
+>>>>>>> Stashed changes
 
 export default function Home({ navigation }, props) {
   return (
     <Pantalla>
+<<<<<<< Updated upstream
       <TopBar />
+=======
+      <Text>Home</Text>
+      <NavButton 
+          img_src={require('../../assets/bizzumIcon.png')}
+          onPress={() => navigation.navigate("Bizum")}
+      />
+>>>>>>> Stashed changes
     </Pantalla>
   );
 }
